6ff55f475e493f4f97bd57008c07277ae7a3b8ce
Valerie <valeriechin@gmail.com>
2017-06-29 14:46:25 -0700

    updated server.py to use bcrypt

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from validations import formIsValid
 from mysqlconnection import MySQLConnector
-import md5
+from flask_bcrypt import Bcrypt
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 app.secret_key="secretsrunsdeep"
-
 mysql = MySQLConnector(app, 'registration')
 
 
@@ -17,7 +17,7 @@ def index():
 def register():
     state = formIsValid(request.form)
     if (state['isValid']):
-        password = encrypted_password = md5.new(request.form['pw']).hexdigest();
+        password = bcrypt.generate_password_hash(request.form['pw'])
         query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (:first_name, :last_name, :email, :password, NOW(), NOW())"
         data = {
             'first_name' : request.form['first_name'],
@@ -40,14 +40,13 @@ def register():
 
 @app.route('/login', methods=['POST'])
 def login():
-    password = encrypted_password = md5.new(request.form['pw']).hexdigest();
-    query = "SELECT * FROM users WHERE email = :email AND password = :password"
+    password = request.form['pw']
+    query = "SELECT * FROM users WHERE email = :email LIMIT 1"
     data = {
         'email': request.form['email'],
-        'password':password
     }
     result= mysql.query_db(query, data)
-    if len(result) >0:
+    if bcrypt.check_password_hash(result[0]['password'], password):
         return render_template('/profile.html', user=result[0])
     else:
         flash("Couldn't find you.")
